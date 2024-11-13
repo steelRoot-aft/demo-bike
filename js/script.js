1510d410a98da2134f6a9440b8e98fab0b6b052b
steelRoot <aleksandrburban478@gmail.com>
2024-11-13 22:57:12 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,46 @@
+$('.icon-menu').click(function (event) {
+	$(this).toggleClass('active');
+	$('.menu__body').toggleClass('active');
+	$('body').toggleClass('lock');
+});
+
+$('.menu__body a').click(function () {
+	$('.icon-menu, .menu__body').removeClass('active');
+	$('body').removeClass('lock');
+});
+
+function ibg(){
+		$.each($('.ibg'), function(_index, val) {
+			if($(this).find('img').length>0){
+				$(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
+			}
+		});
+}
+ibg();
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("mySlides");
+	var dots = document.getElementsByClassName("dot");
+	if (n > slides.length) { slideIndex = 1 }
+	if (n < 1) { slideIndex = slides.length }
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+	slides[slideIndex - 1].style.display = "block";
+	dots[slideIndex - 1].className += " active";
+}
